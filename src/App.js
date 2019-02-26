48c0aa07fe7ab98f2b52c8e26b4a42f76051da54
Gabriel Kutsovsky <gkutsovsky@gmail.com>
2019-02-25 20:24:19 -0500

    committing

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Main from './components/Main';
+import Resume from './components/Resume';
+import SocialLinks from './components/SocialLinks';
+import Projects from './components/Projects'; 
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Home />
+        <Main />
+        <Projects />
+        <Resume />
+        <Contact />
+        <SocialLinks />
+        <Footer /> 
+      </div>
+    );
+  }
+}
+
+export default App;
